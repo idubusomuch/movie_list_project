b32d12fb99fee60058ae125ea9781bf9d93d9554
idubusomuch <idubusomuch@gmail.com>
2025-05-16 13:05:50 +0900

    회원가입 및 로그인 페이지에도 다크모드 적용

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -69,8 +69,8 @@ export default function Login() {
 
   return (
     <>
-      <div className='min-h-screen bg-gray-100 py-8 px-4'>
-        <div className='max-w-xl mx-auto bg-white shadow-lg rounded-lg p-6'>
+      <div className='h-full bg-skin py-8 px-4'>
+        <div className='max-w-xl mx-auto shadow-2xl rounded-lg p-6'>
           <div className='flex justify-center mb-6'></div>
           <h1 className='text-3xl font-bold text-center mb-6'>로그인</h1>
           <form onSubmit={handleSubmit}>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -67,8 +67,8 @@ export default function Signup() {
   }
 
   return (
-    <div className='min-h-screen bg-gray-100 py-8 px-4'>
-      <div className='max-w-xl mx-auto bg-white shadow-lg rounded-lg p-6'>
+    <div className='h-full bg-skin py-8 px-4'>
+      <div className='max-w-xl mx-auto shadow-xl rounded-lg p-6'>
         <div className='flex justify-center mb-6'></div>
         <h1 className='text-3xl font-bold text-center mb-6'>회원가입</h1>
         <form onSubmit={handleSubmit}>
